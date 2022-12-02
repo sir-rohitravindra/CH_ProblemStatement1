10cfbaf2fd0099b2efc925e6e29e0e8333c71f35
Rohit <rohit.ravindra007@gmail.com>
2022-12-02 17:55:23 +0530

    app running with docker compose

--- app/app.py
+++ app/app.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-user = ''           # username as set for the mongodb admin server (the username used in secret.yaml - before base64 conversion)
-password = ''       # password as set for the mongodb admin server (the password used in secret.yaml - before base64 conversion)
-host = ''    # service name of the mongodb admin server as set in the service for mongodb server
-port = ''              # port number of the mongodb admin server as set in the service for mongodb server
+user = 'root'           # username as set for the mongodb admin server (the username used in secret.yaml - before base64 conversion)
+password = 'admin'       # password as set for the mongodb admin server (the password used in secret.yaml - before base64 conversion)
+host = 'mongodb'    # service name of the mongodb admin server as set in the service for mongodb server
+port = '27017'              # port number of the mongodb admin server as set in the service for mongodb server
 conn_string = f'mongodb://{user}:{password}@{host}:{port}'
 
 db = MongoClient(conn_string).blog
